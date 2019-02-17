1decb8e0c291d5e73c1e6b33ae0bfd936796f2bc
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-17 02:52:30 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -67,15 +67,16 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if string.toLowerCase() == lowercase;
+  
   return "I can't hear you";
-}
 
-function sayHiToGrandma(string){
-  if string.toUpperCase() == uppercase;
+  else if string.toUpperCase() == uppercase;
+
   return "YES INDEED!";
+
+  else if string === "I love you, Grandma";
+
+  return "I love you, too";
+  
 }
 
-function sayHiToGrandma(string){
-  if string == "I love you, Grandma.";
-  return "I love you, too";
-}
